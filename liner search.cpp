a0034e5edf08ae3b0197b0966481a2cc0b9951e9
Bharat Gurbaxani <gurbaxanibharat@gmail.com>
2018-03-10 00:23:24 +0530

    Implementation using function added.

--- liner search.cpp
+++ liner search.cpp
@@ -21,3 +21,34 @@ int main(){
   if (count==0) cout<<"Element not found";
   return 0;
 }
+
+-----------------------------------------------------------------------------------------------------------------------------------------
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+#define loop(i,n) for(int i=0;i<n;i++)
+
+int linear(int a[],int n,int key){
+  int count=0;
+  loop(i,n){
+    if(key == a[i]){
+      cout<<"Element "<<key<<" found at "<<i+1<<" position";
+      count++;
+      break;
+    }
+  }
+    if (count==0) cout<<"Element not found";
+  }
+
+int main(){
+  int array[10]={1,2,3,4,5,5,7,8,9,10};
+  int n = sizeof(array)/sizeof(array[0]);
+  //loop(i,n) cin>>array[i];
+
+  int key;
+  cin>>key;
+  linear(array,n,key);
+  return 0;
+}
