a6e4a3c81763a96ba6b2890277f8552a21762a9d
Bharat Gurbaxani <bharat.gurbaxani@highradius.com>
2020-02-05 18:28:54 +0530

    Basic Code to add two given numbers

---
+++ SUM OF TWO NUMBERS.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    cout << "Please input 2 number"<<endl;
+    cin>> a >> b;
+    cout << "sum :" << (a+b);
+    return 0;
+}
