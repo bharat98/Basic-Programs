eb0c4515ca46f946c0b1aa1e22635556d73aba56
Bharat Gurbaxani <gurbaxanibharat@gmail.com>
2020-02-13 19:35:31 +0530

    Update Py Basics.py

--- Py Basics.py
+++ Py Basics.py
@@ -4,11 +4,11 @@
 One file notes of all the basics acquired 
 during the process of learning
 '''
-integer_var=5         # Defining an integer
+integer_var=5                   # Defining an integer
 print(integer_var)
-float_var=1.13        # Defining a float
+float_var=1.13                  # Defining a float
 print(float_var)
-mystring = 'hello'    #Strings are defined either with a single quote or a double quotes.
+mystring = 'hello'              #Strings are defined either with a single quote or a double quotes.
 print(mystring)
 mystring = "hello"
 print(mystring)
