d659edad49b763e4fec5da9eb728957b3db6e75f
Bharat Gurbaxani <bharat.gurbaxani@highradius.com>
2020-02-13 16:49:01 +0530

    Pyhton Basics

---
+++ Py Basics.py
@@ -0,0 +1,18 @@
+# Basic Python Guide
+
+''' 
+One file notes of all the basics acquired 
+during the process of learning
+'''
+integer_var=5         # Defining an integer
+print(integer_var)
+float_var=1.13        # Defining a float
+print(float_var)
+mystring = 'hello'    #Strings are defined either with a single quote or a double quotes.
+print(mystring)
+mystring = "hello"
+print(mystring)
+mystring = "Don't confuse"      #The difference between the two is that using double quotes makes it easy to include apostrophes.
+print(mystring)
+
+
